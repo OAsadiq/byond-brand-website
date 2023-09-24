09ed9e8117e8a1b6be387faab0be490b5d43cf7f
Sadiq <104064346+OAsadiq@users.noreply.github.com>
2023-09-24 01:25:10 +0100

    initial commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Neer COllection Gown',
+        productNumber: '89673',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Neer COllection Gown',
+        productNumber: '89683',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Neer COllection Gown',
+        productNumber: '89674',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Neer COllection Gown',
+        productNumber: '89693',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Neer COllection Gown',
+        productNumber: '89675',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Neer COllection Gown',
+        productNumber: '89703',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    }
+]
